459171c88ab4030b8821016924ae0228af11a609
Sinan Alakhverdovi <sinanallaxverdov07@gmail.com>
2024-11-13 15:41:56 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-// Write your code here
+// დავალებები
+// 1
+let num1 = 3; 
+let num2 = 7; 
+let sum = num1 + num2; 
+console.log("ჯამი იქნება: " + sum);
+
+// 2
+let num3 = 5; 
+let num4 = 8; 
+let product = num3 * num4; 
+console.log("ნამრავლი იქნება: " + product);
+
+// 3
+let myname = "sinan";
+console.log("hello "+ myname);
+
+// 4
+let string1 = "HELLO";
+let string2 = "WORLD";
+let result = string1 + " " +string2;
+console.log("შედეგი იქნება "+ result);
+
+
+
+
